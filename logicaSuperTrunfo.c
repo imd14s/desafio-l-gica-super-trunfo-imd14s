9a9aa44a304f8c9889e8053cd66ec26a48b3e843
imd14s <eversond14s@outlook.com>
2025-04-28 17:29:16 +0000

    end of the first challenge

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+// struct da cidade.
 typedef struct {
     char state[50]; 
     char id_card[50];
@@ -15,6 +12,7 @@ typedef struct {
     int tourist_spot_numbers;
 } Card;
 
+// função que recebe a struct de carta para interagir com usuario e adicionar a carta.
 void add_attributes(Card *new_card) {
     printf("Digite o nome da cidade: \n");
     scanf("%s", &new_card->city);
@@ -38,47 +36,56 @@ void add_attributes(Card *new_card) {
     scanf("%d", &new_card->tourist_spot_numbers);
 };
 
-// estado, código da carta, nome da cidade, população, área, PIB e número de pontos turísticos
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+
     Card card_one;
     Card card_two;
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    // variavel onde será armazenado a pontuação da cidade.
+    int play_one, play_two;
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
-    
-    // função que recebe a struct de carta para interagir com usuario e adicionar a carta.
 
     add_attributes(&card_one);
+    printf(" \n \n adicionado carta 2 \n \n ");
     add_attributes(&card_two);
 
-    printf("cidade 1: %s \n", card_one.city);
-    printf("cidade 2 %s \n", card_two.city);
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    if (card_one.population < card_two.population) {
+        printf("A cidade %s tem a menor densidade populacional. \n", card_one.city);
+        play_one = play_one + 1;
+    } else {
+        printf("A cidade %s tem a menor densidade populacional. \n", card_two.city);
+        play_two =  play_two + 1;
+    }
+    if (card_one.field > card_two.field) {
+        printf("A cidade %s tem maior área. \n", card_one.city);
+        play_one = play_one + 1;
+    } else {
+        printf("A cidade %s tem maior área. \n", card_two.city);
+        play_two =  play_two + 1;
+    }
+    if (card_one.pib > card_two.pib) {
+        printf("A cidade %s tem maior PIB. \n", card_one.city);
+        play_one = play_one + 1;
+    } else {
+        printf("A cidade %s tem maior PIB. \n", card_two.city);
+        play_two =  play_two + 1;
+    }
+    if (card_one.tourist_spot_numbers > card_two.tourist_spot_numbers) {
+        printf("A cidade %s tem mais pontos turisticos. \n", card_one.city);
+        play_one = play_one + 1;
+    } else {
+        printf("A cidade %s tem mais pontos turisticos. \n", card_two.city);
+        play_two =  play_two + 1;
+    }
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    // verificando vencedor.
+    if(play_one > play_two) {
+        printf("A cidade vencedora é: %s\n", card_one.city);
+    } else if (play_one > play_two) {
+        printf("A cidade vencedora é: %s\n", card_two.city);
+    } else {
+        prinft("As cidade empataram!")
+    }
 
     return 0;
 }
